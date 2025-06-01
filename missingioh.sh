13510c9e3c670727d0915245e347f980bc046f76
df7be <df7be@66345640-f411-406e-acba-706ed0d45c9c>
2025-06-01 17:02:41 +0000

    Raspberry Pi installation, troubleshooting missing include file sys/io.h
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@3778 66345640-f411-406e-acba-706ed0d45c9c

---
+++ missingioh.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# $Id$
+#
+# This script installs a workaround for missing sys/io.h
+# (by DF7BE) 
+#
+# This include file is part of old versions of GLIBC and is now removed.
+# Instead, the file <unistd.h> must be included.
+#
+# The origin location of include files.
+SYS_INCLUDEDIR=/usr/include
+# 
+# On some *NIX sytems the location may be here,
+# so activate this setting.
+# SYS_INCLUDEDIR=/usr/local/include
+#
+# It checks the existance of sys/io.h,
+# if not, it copies the unistd.h as io.h
+# into the HWGUI directory include/sys
+#  into the HWGUI directory include/sys,
+# so no modification of HWGUI build scripts
+# are necessary.
+# For more information see the installation instructions
+# of HWGUI.
+if [ ! -f $SYS_INCLUDEDIR/sys/io.h ]
+  then
+  if [ ! -f include/sys/io.h ]
+  then
+   echo "Start workaround ..."
+   cp $SYS_INCLUDEDIR/unistd.h include/sys/io.h
+   if [ $? -ne 0 ]
+   then
+     echo "Error"
+   else  
+     echo "Done"
+   fi
+   else
+    echo "OK: Nothing to do !"
+   fi
+else
+ echo "OK: sys/io.h on system found"
+fi
+
+# ===================== EOF of missingioh.sh ===============================
+
