e036d3b82d29f4b05649392deae577936be69488
df7be <df7be@66345640-f411-406e-acba-706ed0d45c9c>
2026-04-13 14:42:42 +0000

    Project iconification sample:Terminal miss start of Helloworld.prg\ Fixed.To be continued
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@3822 66345640-f411-406e-acba-706ed0d45c9c

---
+++ samples/iconify/start_helloworld.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# export PWD=`pwd` 
+# echo $PWD > pwd.txt
+# lxterminal --geometry=80x25 -e "cd $PWD ; ./helloworld 1>&2 2>mist.txt" 
+./helloworld
+# Use this for debugging (e.g. no start)
+# ./log 1>&2 2>mist.txt
