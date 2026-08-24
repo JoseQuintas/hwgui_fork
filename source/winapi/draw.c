e25fdb8720df185d5c0da681ab23dc006fae0be6
itamarlins <itamarlins@66345640-f411-406e-acba-706ed0d45c9c>
2026-08-24 18:52:46 +0000

    Fixes draw.c: Fixed GDI object leak and potential freeze loop in HWG_SPREADBITMAP. Enforced object handle restoration using hOldObj and safe size boundary validations to preserve thread stability in 64-bit Clang configurations.
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@3866 66345640-f411-406e-acba-706ed0d45c9c

--- source/winapi/draw.c
+++ source/winapi/draw.c
@@ -1049,45 +1049,56 @@ HB_FUNC( HWG_DRAWTRANSPARENTBITMAP )
             DeleteDC( dcTrans );
 }
 
-/*  SpreadBitmap( hDC, hBitmap [, nLeft, nTop, nRight, nBottom] )
-*/
+/*
+ * SpreadBitmap( hDC, hBitmap [, nLeft, nTop, nRight, nBottom] )
+ */
 HB_FUNC( HWG_SPREADBITMAP )
 {
-   HDC hDC = ( HDC ) HB_PARHANDLE( 1 );
-   HDC hDCmem = CreateCompatibleDC( hDC );
-   //DWORD dwraster = ( HB_ISNIL( 3 ) ) ? SRCCOPY : ( DWORD ) hb_parnl( 3 );
-   HBITMAP hBitmap = ( HBITMAP ) HB_PARHANDLE( 2 );
-   BITMAP bitmap;
-   RECT rc;
-   int nLeft, nWidth, nHeight;
+      HDC hDC = ( HDC ) HB_PARHANDLE( 1 );
+      HDC hDCmem = CreateCompatibleDC( hDC );
+      HBITMAP hBitmap = ( HBITMAP ) HB_PARHANDLE( 2 );
+      BITMAP bitmap;
+      RECT rc;
+      int nLeft, nWidth, nHeight;
 
-   rc.left = (HB_ISNUM(3))? hb_parni(3) : 0;
-   rc.top = (HB_ISNUM(4))? hb_parni(4) : 0;
-   rc.right = (HB_ISNUM(5))? hb_parni(5) : 0;
-   rc.bottom = (HB_ISNUM(6))? hb_parni(6) : 0;
+      rc.left   = ( HB_ISNUM( 3 ) ) ? hb_parni( 3 ) : 0;
+      rc.top    = ( HB_ISNUM( 4 ) ) ? hb_parni( 4 ) : 0;
+      rc.right  = ( HB_ISNUM( 5 ) ) ? hb_parni( 5 ) : 0;
+      rc.bottom = ( HB_ISNUM( 6 ) ) ? hb_parni( 6 ) : 0;
 
-   SelectObject( hDCmem, hBitmap );
-   GetObject( hBitmap, sizeof( BITMAP ), ( LPVOID ) & bitmap );
-   if( rc.left == 0 && rc.right == 0 )
-      GetClientRect( WindowFromDC( hDC ), &rc );
+      // Save the original default bitmap to prevent memory leak
+      HGDIOBJ hOldObj = SelectObject( hDCmem, hBitmap );
 
-   nLeft = rc.left;
-   while( rc.top < rc.bottom )
-   {
-      nHeight = (rc.bottom-rc.top >= bitmap.bmHeight)? bitmap.bmHeight : rc.bottom-rc.top;
-      while( rc.left < rc.right )
+      if( GetObject( hBitmap, sizeof( BITMAP ), ( LPVOID ) & bitmap ) == 0 ||
+            bitmap.bmWidth <= 0 || bitmap.bmHeight <= 0 )
       {
-         nWidth = (rc.right-rc.left >= bitmap.bmWidth)? bitmap.bmWidth : rc.right-rc.left;
-         BitBlt( hDC, rc.left, rc.top, nWidth, nHeight, hDCmem, 0, 0, SRCCOPY );
-         rc.left += bitmap.bmWidth;
+            // Safe exit if the bitmap properties are invalid to prevent infinite loop
+            SelectObject( hDCmem, hOldObj );
+            DeleteDC( hDCmem );
+            return;
       }
-      rc.left = nLeft;
-      rc.top += bitmap.bmHeight;
-   }
 
-   DeleteDC( hDCmem );
+      if( rc.left == 0 && rc.right == 0 )
+            GetClientRect( WindowFromDC( hDC ), &rc );
+
+      nLeft = rc.left;
+      while( rc.top < rc.bottom )
+      {
+            nHeight = ( rc.bottom - rc.top >= bitmap.bmHeight ) ? bitmap.bmHeight : rc.bottom - rc.top;
+            while( rc.left < rc.right )
+            {
+                  nWidth = ( rc.right - rc.left >= bitmap.bmWidth ) ? bitmap.bmWidth : rc.right - rc.left;
+                  BitBlt( hDC, rc.left, rc.top, nWidth, nHeight, hDCmem, 0, 0, SRCCOPY );
+                  rc.left += bitmap.bmWidth;
+            }
+            rc.left = nLeft;
+            rc.top += bitmap.bmHeight;
+      }
+
+      // Restore the original bitmap before deleting the memory device context
+      SelectObject( hDCmem, hOldObj );
+      DeleteDC( hDCmem );
 }
-
 
 /*  CenterBitmap( hDC, hWnd, hBitmap, style, brush )
 */
