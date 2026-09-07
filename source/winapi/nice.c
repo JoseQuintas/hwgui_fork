231bb51f3ffd23ece0d612d47d15aea0d3518db1
itamarlins <itamarlins@66345640-f411-406e-acba-706ed0d45c9c>
2026-09-07 05:42:59 +0000

    ### Fixed   - **nice.c**: fixed 64-bit compatibility – replaced `GetWindowLong()`     with `GetWindowLongPtr()` in `HWG_GETWINDOWLONG()`.
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@3916 66345640-f411-406e-acba-706ed0d45c9c

--- source/winapi/nice.c
+++ source/winapi/nice.c
@@ -2,7 +2,7 @@
  * $Id$
  *
  * HWGUI - Harbour Win32 GUI library source code:
- * 
+ * Nice button with gradient effects
  *
  * Copyright 2003 Luiz Rafael Culik Guimaraes <culikr@brtrubo.com>
  * www - http://sites.uol.com.br/culikr/
@@ -17,92 +17,88 @@
 
 #include "incomp_pointer.h"
 
-#ifndef GRADIENT_FILL_RECT_H
+/* REMOVED: Obsolete compiler support - GRADIENT_RECT and TRIVERTEX
+ * are already defined in modern Windows headers (wingdi.h)
+ * #if !defined(__WATCOMC__) && !defined(__MINGW32__) && !defined(__MINGW64__)
+ * typedef struct _GRADIENT_RECT { ... } GRADIENT_RECT, ...
+ * #endif
+ * #if defined(__DMC__)
+ * typedef struct _TRIVERTEX { ... } TRIVERTEX, ...
+ * #endif
+ */
 
-#define GRADIENT_FILL_RECT_H 0
-#define GRADIENT_FILL_RECT_V 1
-
-#if !defined(__WATCOMC__) && !defined(__MINGW32__) && !defined(__MINGW64__)
-typedef struct _GRADIENT_RECT
-{
-   ULONG UpperLeft;
-   ULONG LowerRight;
-} GRADIENT_RECT, *PGRADIENT_RECT, *LPGRADIENT_RECT;
-#endif
-
-#if defined(__DMC__)
-typedef struct _TRIVERTEX
-{
-   LONG x;
-   LONG y;
-   USHORT Red;
-   USHORT Green;
-   USHORT Blue;
-   USHORT Alpha;
-} TRIVERTEX, *PTRIVERTEX, *LPTRIVERTEX;
-#endif
-
-#endif
-
-typedef int ( _stdcall * GRADIENTFILL ) ( HDC, PTRIVERTEX, int, PVOID, int,
-      int );
+typedef int ( _stdcall * GRADIENTFILL ) ( HDC, PTRIVERTEX, int, PVOID, int, int );
 LRESULT CALLBACK NiceButtProc( HWND, UINT, WPARAM, LPARAM );
 
 static GRADIENTFILL s_pGradientfill = NULL;
 
+/*=============================================================================
+ * Draw_Gradient()
+ * Draws a gradient with a single color fading to white
+ *===========================================================================*/
 void Draw_Gradient( HDC hdc, int x, int y, int w, int h, int r, int g, int b )
 {
    TRIVERTEX Vert[2];
    GRADIENT_RECT Rect;
    HB_SYMBOL_UNUSED( x );
    HB_SYMBOL_UNUSED( y );
-   // ******************************************************
+
+   if( !s_pGradientfill )
+      return;
+
    Vert[0].x = 0;
    Vert[0].y = 0;
    Vert[0].Red = 65535 - ( 65535 - ( r * 256 ) );
    Vert[0].Green = 65535 - ( 65535 - ( g * 256 ) );
    Vert[0].Blue = 65535 - ( 65535 - ( b * 256 ) );
    Vert[0].Alpha = 0;
-   // ******************************************************
+
    Vert[1].x = w;
    Vert[1].y = h / 2;
    Vert[1].Red = 65535 - ( 65535 - ( 255 * 256 ) );
    Vert[1].Green = 65535 - ( 65535 - ( 255 * 256 ) );
    Vert[1].Blue = 65535 - ( 65535 - ( 255 * 256 ) );
    Vert[1].Alpha = 0;
-   // ******************************************************
+
    Rect.UpperLeft = 0;
    Rect.LowerRight = 1;
-   // ******************************************************
+
    s_pGradientfill( hdc, Vert, 2, &Rect, 1, GRADIENT_FILL_RECT_V );
-   // ******************************************************
+
    Vert[0].x = 0;
    Vert[0].y = h / 2;
    Vert[0].Red = 65535 - ( 65535 - ( 255 * 256 ) );
    Vert[0].Green = 65535 - ( 65535 - ( 255 * 256 ) );
    Vert[0].Blue = 65535 - ( 65535 - ( 255 * 256 ) );
    Vert[0].Alpha = 0;
-   // ******************************************************
+
    Vert[1].x = w;
    Vert[1].y = h;
    Vert[1].Red = 65535 - ( 65535 - ( r * 256 ) );
    Vert[1].Green = 65535 - ( 65535 - ( g * 256 ) );
    Vert[1].Blue = 65535 - ( 65535 - ( b * 256 ) );
    Vert[1].Alpha = 0;
-   // ******************************************************
+
    Rect.UpperLeft = 0;
    Rect.LowerRight = 1;
-   // ******************************************************
+
    s_pGradientfill( hdc, Vert, 2, &Rect, 1, GRADIENT_FILL_RECT_V );
 }
 
-void Gradient( HDC hdc, int x, int y, int w, int h, int color1, int color2, int nmode ) //int , int g, int b, int nMode )
+/*=============================================================================
+ * Gradient()
+ * Draws a gradient between two colors
+ *===========================================================================*/
+void Gradient( HDC hdc, int x, int y, int w, int h, int color1, int color2, int nmode )
 {
    TRIVERTEX Vert[2];
    GRADIENT_RECT Rect;
    int r, g, b, r2, g2, b2;
    HB_SYMBOL_UNUSED( x );
    HB_SYMBOL_UNUSED( y );
+
+   if( !s_pGradientfill )
+      return;
 
    r = color1 % 256;
    g = color1 / 256 % 256;
@@ -111,28 +107,30 @@ void Gradient( HDC hdc, int x, int y, int w, int h, int color1, int color2, int 
    g2 = color2 / 256 % 256;
    b2 = color2 / 256 / 256 % 256;
 
-
-   // ******************************************************
    Vert[0].x = 0;
    Vert[0].y = 0;
    Vert[0].Red = 65535 - ( 65535 - ( r * 256 ) );
    Vert[0].Green = 65535 - ( 65535 - ( g * 256 ) );
    Vert[0].Blue = 65535 - ( 65535 - ( b * 256 ) );
    Vert[0].Alpha = 0;
-   // ******************************************************
+
    Vert[1].x = w;
    Vert[1].y = h;
    Vert[1].Red = 65535 - ( 65535 - ( r2 * 256 ) );
    Vert[1].Green = 65535 - ( 65535 - ( g2 * 256 ) );
    Vert[1].Blue = 65535 - ( 65535 - ( b2 * 256 ) );
    Vert[1].Alpha = 0;
-   // ******************************************************
+
    Rect.UpperLeft = 0;
    Rect.LowerRight = 1;
-   // ******************************************************
-   s_pGradientfill( hdc, Vert, 2, &Rect, 1, nmode );    //GRADIENT_FILL_RECT_H );
+
+   s_pGradientfill( hdc, Vert, 2, &Rect, 1, nmode );
 }
 
+/*=============================================================================
+ * NiceButtProc()
+ * Window procedure for nice buttons
+ *===========================================================================*/
 LRESULT CALLBACK NiceButtProc( HWND hWnd, UINT message, WPARAM wParam,
       LPARAM lParam )
 {
@@ -141,13 +139,12 @@ LRESULT CALLBACK NiceButtProc( HWND hWnd, UINT message, WPARAM wParam,
    if( ( pSymTest = hb_dynsymFind( "HWG_NICEBUTTPROC" ) ) != NULL )
    {
       hb_vmPushSymbol( hb_dynsymSymbol( pSymTest ) );
-      hb_vmPushNil(  );         /* places NIL at self */
-      //hb_vmPushLong( ( LONG ) hWnd );   /* pushes parameters on to the hvm stack */
+      hb_vmPushNil();
       HB_PUSHITEM( hWnd );
       hb_vmPushLong( ( LONG ) message );
       hb_vmPushLong( ( LONG ) wParam );
       hb_vmPushLong( ( LONG ) lParam );
-      hb_vmDo( 4 );             /* where iArgCount is the number of pushed parameters */
+      hb_vmDo( 4 );
       res = hb_parl( -1 );
       if( res )
          return 0;
@@ -158,7 +155,10 @@ LRESULT CALLBACK NiceButtProc( HWND hWnd, UINT message, WPARAM wParam,
       return ( DefWindowProc( hWnd, message, wParam, lParam ) );
 }
 
-
+/*=============================================================================
+ * HWG_CREATEROUNDRECTRGN()
+ * Creates a rounded rectangle region
+ *===========================================================================*/
 HB_FUNC( HWG_CREATEROUNDRECTRGN )
 {
    HRGN Res = CreateRoundRectRgn( hb_parni( 1 ), hb_parni( 2 ), hb_parni( 3 ),
@@ -166,24 +166,35 @@ HB_FUNC( HWG_CREATEROUNDRECTRGN )
    HB_RETHANDLE( Res );
 }
 
-
+/*=============================================================================
+ * HWG_SETWINDOWRGN()
+ * Sets window region
+ *===========================================================================*/
 HB_FUNC( HWG_SETWINDOWRGN )
 {
    hb_retni( SetWindowRgn( ( HWND ) HB_PARHANDLE( 1 ), ( HRGN ) hb_parptr( 2 ),
                hb_parl( 3 ) ) );
 }
 
+/*=============================================================================
+ * HWG_REGNICE()
+ * Registers the nice button window class
+ *===========================================================================*/
 HB_FUNC( HWG_REGNICE )
 {
-   // **********[ DLL Declarations ]**********
    static LPCTSTR s_szAppName = TEXT( "NICEBUTT" );
    static BOOL s_bRegistered = 0;
 
-   s_pGradientfill = ( GRADIENTFILL )
-         GetProcAddress( LoadLibrary( TEXT( "MSIMG32.DLL" ) ),
-         "GradientFill" );
-//    if (Gradientfill == NULL)
-//        return FALSE;
+   /* Load GradientFill from msimg32.dll */
+   if( s_pGradientfill == NULL )
+   {
+      HMODULE hMsImg = LoadLibrary( TEXT( "MSIMG32.DLL" ) );
+      if( hMsImg )
+      {
+         s_pGradientfill = ( GRADIENTFILL ) GetProcAddress( hMsImg, "GradientFill" );
+      }
+   }
+
    if( !s_bRegistered )
    {
       WNDCLASS wc;
@@ -204,7 +215,10 @@ HB_FUNC( HWG_REGNICE )
    }
 }
 
-
+/*=============================================================================
+ * HWG_CREATENICEBTN()
+ * Creates a nice button control
+ *===========================================================================*/
 HB_FUNC( HWG_CREATENICEBTN )
 {
    HWND hWndPanel;
@@ -212,19 +226,23 @@ HB_FUNC( HWG_CREATENICEBTN )
          HB_ISNUM( 3 ) ? hb_parnl( 3 ) : WS_CLIPCHILDREN | WS_CLIPSIBLINGS;
    void *hTitle;
 
-   hWndPanel = CreateWindowEx( hb_parni( 8 ), TEXT( "NICEBUTT" ),       /* predefined class  */
-         HB_PARSTR( 9, &hTitle, NULL ), /* no window title   */
-         WS_CHILD | WS_VISIBLE | ulStyle,       /* style  */
-         hb_parni( 4 ), hb_parni( 5 ),  /* x, y       */
-         hb_parni( 6 ), hb_parni( 7 ),  /* nWidth, nHeight */
-         ( HWND ) HB_PARHANDLE( 1 ),    /* parent window    */
-         ( HMENU )(UINT_PTR) hb_parni( 2 ),       /* control ID  */
+   hWndPanel = CreateWindowEx( hb_parni( 8 ), TEXT( "NICEBUTT" ),
+         HB_PARSTR( 9, &hTitle, NULL ),
+         WS_CHILD | WS_VISIBLE | ulStyle,
+         hb_parni( 4 ), hb_parni( 5 ),
+         hb_parni( 6 ), hb_parni( 7 ),
+         ( HWND ) HB_PARHANDLE( 1 ),
+         ( HMENU )(UINT_PTR) hb_parni( 2 ),
          GetModuleHandle( NULL ), NULL );
    hb_strfree( hTitle );
 
    HB_RETHANDLE( hWndPanel );
 }
 
+/*=============================================================================
+ * HWG_ISMOUSEOVER()
+ * Checks if the mouse is over a window
+ *===========================================================================*/
 HB_FUNC( HWG_ISMOUSEOVER )
 {
    RECT Rect;
@@ -234,6 +252,10 @@ HB_FUNC( HWG_ISMOUSEOVER )
    hb_retl( PtInRect( &Rect, Pt ) );
 }
 
+/*=============================================================================
+ * HWG_DRAW_GRADIENT()
+ * Draws a gradient using Draw_Gradient()
+ *===========================================================================*/
 HB_FUNC( HWG_DRAW_GRADIENT )
 {
    Draw_Gradient( ( HDC ) HB_PARHANDLE( 1 ), hb_parni( 2 ), hb_parni( 3 ),
@@ -241,13 +263,18 @@ HB_FUNC( HWG_DRAW_GRADIENT )
          hb_parni( 8 ) );
 }
 
+/*=============================================================================
+ * HWG_GRADIENT()
+ * Draws a gradient between two colors
+ *===========================================================================*/
 HB_FUNC( HWG_GRADIENT )
 {
    if( s_pGradientfill == NULL )
-      s_pGradientfill = ( GRADIENTFILL )
-            GetProcAddress( LoadLibrary( TEXT( "MSIMG32.DLL" ) ),
-            "GradientFill" );
-   //void Gradient( HDC hdc, int x, int y, int w, int h, int color1, int color2, int nmode )
+   {
+      HMODULE hMsImg = LoadLibrary( TEXT( "MSIMG32.DLL" ) );
+      if( hMsImg )
+         s_pGradientfill = ( GRADIENTFILL ) GetProcAddress( hMsImg, "GradientFill" );
+   }
 
    Gradient( ( HDC ) HB_PARHANDLE( 1 ), hb_parni( 2 ), hb_parni( 3 ),
          hb_parni( 4 ), hb_parni( 5 ),
@@ -256,22 +283,33 @@ HB_FUNC( HWG_GRADIENT )
          ( hb_pcount(  ) > 7 && !HB_ISNIL( 8 ) ) ? hb_parni( 8 ) : 0 );
 }
 
+/*=============================================================================
+ * HWG_MAKELONG()
+ * Creates a LONG from two WORDs
+ *===========================================================================*/
 HB_FUNC( HWG_MAKELONG )
 {
    hb_retnl( ( LONG ) MAKELONG( ( WORD ) hb_parnl( 1 ),
                ( WORD ) hb_parnl( 2 ) ) );
 }
 
-
+/*=============================================================================
+ * HWG_GETWINDOWLONG()
+ * Gets window long (using GetWindowLongPtr for 64-bit safety)
+ *===========================================================================*/
 HB_FUNC( HWG_GETWINDOWLONG )
 {
-   hb_retnl( GetWindowLong( ( HWND ) HB_PARHANDLE( 1 ), hb_parni( 2 ) ) );
+   /* FIXED: Use GetWindowLongPtr for 64-bit compatibility */
+   hb_retnint( ( HB_MAXINT ) GetWindowLongPtr( ( HWND ) HB_PARHANDLE( 1 ), hb_parni( 2 ) ) );
 }
 
+/*=============================================================================
+ * HWG_SETBKMODE()
+ * Sets background mode
+ *===========================================================================*/
 HB_FUNC( HWG_SETBKMODE )
 {
    hb_retni( SetBkMode( ( HDC ) HB_PARHANDLE( 1 ), hb_parni( 2 ) ) );
 }
 
 /* =========================== EOF of nice.c ========================== */
-
