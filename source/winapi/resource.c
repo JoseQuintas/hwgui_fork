805e764ed076062b6e036ec6b89086939703d0af
itamarlins <itamarlins@66345640-f411-406e-acba-706ed0d45c9c>
2026-09-07 05:57:29 +0000

    ### Fixed   - **resource.c**: fixed `HWG_FINDRESOURCE()` to support both numeric and     string resource IDs/types – previously only numeric IDs were handled     due to incorrect `IS_INTRESOURCE()` usage.
    git-svn-id: https://svn.code.sf.net/p/hwgui/code/trunk/hwgui@3918 66345640-f411-406e-acba-706ed0d45c9c

--- source/winapi/resource.c
+++ source/winapi/resource.c
@@ -6,13 +6,15 @@
  *
  * Copyright 2003 Luiz Rafael Culik Guimaraes <culikr@brtrubo.com>
  * www - http://sites.uol.com.br/culikr/
-
 */
 
 #include "hwingui.h"
-#if defined(__MINGW32__) || defined(__MINGW64__) || defined(__WATCOMC__)
-#include <prsht.h>
-#endif
+
+/* REMOVED: Unnecessary header
+ * #if defined(__MINGW32__) || defined(__MINGW64__) || defined(__WATCOMC__)
+ * #include <prsht.h>
+ * #endif
+ */
 
 #include "hbapiitm.h"
 #include "hbvm.h"
@@ -23,30 +25,67 @@
 
 HMODULE hModule;
 
+/*=============================================================================
+ * HWG_GETRESOURCES()
+ * Returns the current module handle
+ *===========================================================================*/
 HB_FUNC( HWG_GETRESOURCES )
 {
    HB_RETHANDLE( hModule );
 }
 
+/*=============================================================================
+ * HWG_LOADSTRING()
+ * Loads a string resource from the current module
+ *===========================================================================*/
 HB_FUNC( HWG_LOADSTRING )
 {
    TCHAR buffer[ 2048 ];
-   int iBuffRet = LoadString( ( HINSTANCE ) hModule, ( UINT ) hb_parnl( 2 ),
-                              buffer, 2048 );
-   HB_RETSTRLEN( buffer, iBuffRet );
+   int iBuffRet;
+
+   iBuffRet = LoadString( ( HINSTANCE ) hModule, ( UINT ) hb_parnl( 2 ),
+                          buffer, HB_SIZEOFARRAY( buffer ) );
+
+   if( iBuffRet > 0 )
+      HB_RETSTRLEN( buffer, iBuffRet );
+   else
+      hb_retc( "" );
 }
 
+/*=============================================================================
+ * HWG_LOADRESOURCE()
+ * Loads a module (DLL/EXE) for resource access
+ * 
+ * Parameters:
+ *   1 - Module name (string)
+ * 
+ * Returns:
+ *   Module handle (HMODULE) on success, NULL on error
+ *===========================================================================*/
 HB_FUNC( HWG_LOADRESOURCE )
 {
    void * hString;
-   hModule = GetModuleHandle( HB_PARSTR( 1, &hString, NULL ) );
+   HMODULE hMod;
+
+   hMod = GetModuleHandle( HB_PARSTR( 1, &hString, NULL ) );
    hb_strfree( hString );
+
+   if( hMod )
+   {
+      hModule = hMod;
+      HB_RETHANDLE( hMod );
+   }
+   else
+      HB_RETHANDLE( NULL );
 }
 
+/*=============================================================================
+ * hb_resourcemodules()
+ * Startup initialization: sets default module handle
+ *===========================================================================*/
 void hb_resourcemodules( void * cargo )
 {
    HB_SYMBOL_UNUSED( cargo );
-
    hModule = GetModuleHandle( NULL );
 }
 
@@ -59,7 +98,7 @@ HB_CALL_ON_STARTUP_END( _hwgui_module_init_ )
 #elif defined( HB_DATASEG_STARTUP )
    #define HB_DATASEG_BODY    HB_DATASEG_FUNC( _hwgui_module_init_ )
    #include "hbiniseg.h"
-#elif defined( HB_MSC_STARTUP )  // support for old [x]Harbour version
+#elif defined( HB_MSC_STARTUP )
    #if defined( HB_OS_WIN_64 )
       #pragma section( HB_MSC_START_SEGMENT, long, read )
    #endif
@@ -68,26 +107,68 @@ HB_CALL_ON_STARTUP_END( _hwgui_module_init_ )
    #pragma data_seg()
 #endif
 
+/*=============================================================================
+ * HWG_FINDRESOURCE()
+ * Finds a resource in a module
+ * 
+ * Parameters:
+ *   1 - Module name (string) or NULL for current module
+ *   2 - Resource ID (numeric) or name (string)
+ *   3 - Resource type (numeric) or name (string)
+ * 
+ * Returns:
+ *   Handle to the resource (HRSRC) on success, NULL on error
+ *===========================================================================*/
 HB_FUNC( HWG_FINDRESOURCE )
 {
-   HRSRC hHRSRC;
-   int iName = hb_parni( 2 ); // "WindowsXP.Manifest";
-   int iType = hb_parni( 3 ); // RT_MANIFEST = 24
-   void * hString;
+   HRSRC hHRSRC = NULL;
+   void *hModuleName, *hResName, *hResType;
+   HMODULE hMod = NULL;
 
-   hModule = GetModuleHandle( HB_PARSTR( 1, &hString, NULL ) );
-   hb_strfree( hString );
-
-   if( IS_INTRESOURCE( iName ) )
+   /* FIXED: Get module handle */
+   if( !HB_ISNIL( 1 ) )
    {
-      hHRSRC = FindResource( ( HMODULE ) hModule,
-                             MAKEINTRESOURCE( iName ),
-                             MAKEINTRESOURCE( iType ) );
-      HB_RETHANDLE( hHRSRC );
+      hMod = GetModuleHandle( HB_PARSTR( 1, &hModuleName, NULL ) );
+      hb_strfree( hModuleName );
    }
    else
-      HB_RETHANDLE( 0 );
+      hMod = hModule;
+
+   if( !hMod )
+   {
+      HB_RETHANDLE( NULL );
+      return;
+   }
+
+   /* FIXED: Determine if resource name is numeric or string */
+   if( HB_ISNUM( 2 ) )
+   {
+      int iName = hb_parni( 2 );
+      hResName = ( LPTSTR ) MAKEINTRESOURCE( iName );
+   }
+   else
+   {
+      hResName = ( LPTSTR ) HB_PARSTR( 2, &hResName, NULL );
+   }
+
+   /* FIXED: Determine if resource type is numeric or string */
+   if( HB_ISNUM( 3 ) )
+   {
+      int iType = hb_parni( 3 );
+      hResType = ( LPTSTR ) MAKEINTRESOURCE( iType );
+   }
+   else
+   {
+      hResType = ( LPTSTR ) HB_PARSTR( 3, &hResType, NULL );
+   }
+
+   hHRSRC = FindResource( hMod, hResName, hResType );
+
+   /* FIXED: Free string resources if they were allocated */
+   if( HB_ISCHAR( 2 ) )
+      hb_strfree( hResName );
+   if( HB_ISCHAR( 3 ) )
+      hb_strfree( hResType );
+
+   HB_RETHANDLE( hHRSRC );
 }
-
-/* =============================== EOF of resource.c ==================================== */
-
